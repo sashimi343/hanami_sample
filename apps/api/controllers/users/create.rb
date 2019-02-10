16e232bd070dbf4d46d69954c13774e9a411b7aa
Kohei Kakimoto <naige.1028@gmail.com>
2019-02-10 18:07:04 +0900

    Implement user creation action

--- apps/api/controllers/users/create.rb
+++ apps/api/controllers/users/create.rb
@@ -4,7 +4,20 @@ module Api
       class Create
         include Api::Action
 
+        expose :user
+        expose :account
+        expose :errors
+
         def call(params)
+          result = AccountsInteractor::Register.new(params).call
+
+          if result.successful?
+            @user = result.user
+            @account = result.account
+          else
+            self.status = 400
+            @errors = result.errors
+          end
         end
       end
     end
