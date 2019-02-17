274e2515cded3f829dac83220529a3dd03c4d8c1
Kohei Kakimoto <naige.1028@gmail.com>
2019-02-17 13:40:39 +0900

    Enable sessions

--- apps/api/application.rb
+++ apps/api/application.rb
@@ -82,7 +82,7 @@ module Api
       #
       # See: http://www.rubydoc.info/gems/rack/Rack/Session/Cookie
       #
-      # sessions :cookie, secret: ENV['API_SESSIONS_SECRET']
+      sessions :cookie, secret: ENV['API_SESSIONS_SECRET']
 
       # Configure Rack middleware for this application
       #
